54334e2f35dda0365755f866748d78e1723e1afd
Luciano Charles de Souza <souzacomprog@gmail.com>
2019-10-26 23:54:54 -0300

    Update master.blade.php

--- resources/views/fullcalendar/template/master.blade.php
+++ resources/views/fullcalendar/template/master.blade.php
@@ -2,45 +2,27 @@
 <html lang="pt-br">
 <head>
     <meta charset='utf-8'/>
-    <link href='{{asset('assets/fullcalendar/packages/core/main.css')}}' rel='stylesheet'/>
-    <link href='{{asset('assets/fullcalendar/packages/daygrid/main.css')}}' rel='stylesheet'/>
-    <link href='{{asset('assets/fullcalendar/packages/timegrid/main.css')}}' rel='stylesheet'/>
-    <link href='{{asset('assets/fullcalendar/packages/list/main.css')}}' rel='stylesheet'/>
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <link href='{{asset('assets/fullcalendar/css/style.css')}}' rel='stylesheet'/>
-
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>FullCalendar 4.3 with Laravel</title>
+    <title>FullCalendar 4.3 with Laravel And NPM</title>
+
+    <link href='{{url(mix('assets/fullcalendarNPM/css/fullcalendar.css'))}}' rel='stylesheet'/>
+    <link href='{{url(mix('assets/fullcalendarNPM/css/bootstrap.css'))}}' rel='stylesheet'/>
+    <link href='{{url(mix('assets/fullcalendarNPM/css/style.css'))}}' rel='stylesheet'/>
+
 </head>
 <body>
 
 <div id='wrap'>@yield('content')</div>
+
 <div style='clear:both'></div>
 
-<script src='{{asset('assets/fullcalendar/packages/core/main.js')}}'></script>
-<script src='{{asset('assets/fullcalendar/packages/interaction/main.js')}}'></script>
-<script src='{{asset('assets/fullcalendar/packages/daygrid/main.js')}}'></script>
-<script src='{{asset('assets/fullcalendar/packages/timegrid/main.js')}}'></script>
-<script src='{{asset('assets/fullcalendar/packages/list/main.js')}}'></script>
+<script src='{{asset('assets/fullcalendarNPM/js/fullcalendar.js')}}'></script>
+<script src='{{asset('assets/fullcalendarNPM/js/jquery-and-mask-and-moment.js')}}'></script>
+<script src='{{asset('assets/fullcalendarNPM/js/bootstrap.js')}}'></script>
 
-<script src='{{asset('assets/fullcalendar/packages/core/locales-all.js')}}'></script>
 
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-
-<script src='{{asset('assets/fullcalendar/js/script.js')}}'></script>
-
-<script src='{{asset('assets/fullcalendar/js/calendar.js')}}'></script>
-
+<script src='{{asset('assets/fullcalendarNPM/js/scriptCalendar.js')}}'></script>
 
 </body>
 </html>
-
